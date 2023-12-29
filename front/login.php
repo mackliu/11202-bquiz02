7fd4bd4e1150add79d5e28df01ffb8b071c62f18
kai <cgk0910@gmail.com>
2023-12-29 11:34:03 +0800

    建置會員登入畫面

---
+++ front/login.php
@@ -0,0 +1,19 @@
+<fieldset>
+    <legend>會員登入</legend>
+    <table>
+        <tr>
+            <td class='clo'>帳號</td>
+            <td><input type="text" name="" id=""></td>
+        </tr>
+        <tr>
+            <td class='clo'>密碼</td>
+            <td><input type="password" name="" id=""></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="登入"><input type="reset" value="清險"></td>
+            <td>
+                <a href="?do=forget">忘記密碼</a> | <a href="?do=reg">尚未註冊</a>
+            </td>
+        </tr>
+    </table>
+</fieldset>
