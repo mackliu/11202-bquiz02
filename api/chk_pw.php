6f766af75425a2ce67781886882a0a60363350a8
kai <cgk0910@gmail.com>
2023-12-29 15:18:52 +0800

    完成會員登入功能

---
+++ api/chk_pw.php
@@ -0,0 +1,13 @@
+<?php include_once "db.php";
+
+echo $User->count($_POST);
+//echo $User->count(['acc'=>$_POST['acc'],'pw'=>$_POST['pw']]);
+
+/* echo $res; */
+
+/* if($res>0){
+    echo 1;
+}else{
+    echo 0;
+} */
+
--- front/login.php
+++ front/login.php
@@ -11,7 +11,7 @@
         </tr>
         <tr>
             <td>
-                <input type="submit" value="登入">
+                <input type="button" value="登入" onclick="login()">
                 <input type="reset" value="清險"></td>
             <td>
                 <a href="?do=forget">忘記密碼</a> | <a href="?do=reg">尚未註冊</a>
@@ -20,5 +20,23 @@
     </table>
 </fieldset>
 <script>
-
+function login(){
+    $.post('./api/chk_acc.php',{acc:$("#acc").val()},(res)=>{
+        if(parseInt(res)==0){
+            alert("查無帳號")
+        }else{
+            $.post('./api/chk_pw.php',{acc:$("#acc").val(),pw:$("#pw").val()},(res)=>{
+                if(parseInt(res)==1){
+                    if($("#acc").val()=='admin'){
+                        location.href='back.php'
+                    }else{
+                        location.href='index.php'
+                    }
+                }else{
+                    alert("密碼錯誤")
+                }
+            })
+        }
+    })
+}
 </script>
