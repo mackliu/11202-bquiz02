16544928c60bfc9b2dbeb70cbde6cdfc747edb21
kai <cgk0910@gmail.com>
2024-01-05 09:35:21 +0800

    完成主題內容消息區點選切換功能

--- front/main.php
+++ front/main.php
@@ -14,8 +14,9 @@
 
     article section{
         border:1px solid black;
-        min-height:400px;
+        min-height:480px;
         margin-top:-1px;
+        display: none;
     }
     .active{
         border-bottom:1px solid white;
@@ -26,21 +27,32 @@
 
 
 <div class="tags">
-    <div class='tag active'>健康新知</div>
-    <div class='tag'>菸害防治</div>
-    <div class='tag'>癌症防治</div>
-    <div class='tag'>慢性病防治</div>
+    <div id="sec01" class='tag active'>健康新知</div>
+    <div id="sec02" class='tag'>菸害防治</div>
+    <div id="sec03" class='tag'>癌症防治</div>
+    <div id="sec04" class='tag'>慢性病防治</div>
 </div>
+
+
+
+
+
 <article>
-    <section>dfasdf</section>
-    <section>aaaa</section>
-    <section>bbb</section>
-    <section>ccc</section>
+    <section id="section01" style="display:block"><h2>健康新知</h2></section>
+    <section id="section02"><h2>菸害防治</h2></section>
+    <section id="section03"><h2>癌症防治</h2></section>
+    <section id="section04"><h2>慢性病防治</h2></section>
 </article>
 
 <script>
    $(".tag").on('click',function(){
         $(".tag").removeClass('active')
         $(this).addClass('active')
+        let id=$(this).attr('id');
+        console.log(id)
+        let newId=id.replace("sec",'section');
+        console.log(newId)
+        $("section").hide();
+        $("#"+newId).show();
    })
 </script>
