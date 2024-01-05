8b574fb72a6aa0867c0eddefdf1f8d5a90e50e3d
kai <cgk0910@gmail.com>
2024-01-05 10:12:44 +0800

    頁籤增加背景顏色更換功能

--- front/main.php
+++ front/main.php
@@ -6,6 +6,7 @@
         margin-left: -1px;
         border-radius: 5px 5px 0 0;
         text-align: center;
+        background-color: #ccc;
     }
     .tags{
         display: flex;
@@ -22,6 +23,7 @@
     }
     .active{
         border-bottom:1px solid white;
+        background-color: white;
     }
 </style>
 
