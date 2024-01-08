412677715c45a4333c0ea780221d33b76b7e79da
mackliu <macklun@gmail.com>
2024-01-08 09:50:45 +0800

    在最新文章和人氣文章製作分頁功能

--- front/news.php
+++ front/news.php
@@ -7,7 +7,13 @@
             <th></th>
         </tr>
         <?php 
-        $rows=$News->all(['sh'=>1]);
+        $total=$News->count(['sh'=>1]);
+        $div=5;
+        $pages=ceil($total/$div);
+        $now=$_GET['p']??1;
+        $start=($now-1)*$div;
+
+        $rows=$News->all(['sh'=>1]," limit $start,$div");
         foreach($rows as $row){
         ?>
         <tr>
@@ -19,4 +25,20 @@
         }
         ?>
     </table>
+    <div>
+    <?php
+    if(($now-1)>0){
+        $prev=$now-1;
+        echo "<a href='?do=news&p=$prev'> < </a>";
+    }
+    for($i=1;$i<=$pages;$i++){
+        $fontsize=($now==$i)?"font-size:20px":"font-size:16px";
+        echo "<a href='?do=news&p=$i' style='$fontsize'> $i </a>";
+    }
+    if(($now+1)<=$pages){
+        $next=$now+1;
+        echo "<a href='?do=news&p=$next'> > </a>";
+    }
+    ?>
+    </div>
 </fieldset>
--- front/pop.php
+++ front/pop.php
@@ -7,7 +7,13 @@
             <th>人氣</th>
         </tr>
         <?php 
-        $rows=$News->all(['sh'=>1]," order by `good` desc");
+        $total=$News->count(['sh'=>1]);
+        $div=5;
+        $pages=ceil($total/$div);
+        $now=$_GET['p']??1;
+        $start=($now-1)*$div;
+
+        $rows=$News->all(['sh'=>1]," order by `good` desc limit $start,$div");
         foreach($rows as $row){
         ?>
         <tr>
@@ -19,4 +25,20 @@
         }
         ?>
     </table>
+    <div>
+    <?php
+    if(($now-1)>0){
+        $prev=$now-1;
+        echo "<a href='?do=pop&p=$prev'> < </a>";
+    }
+    for($i=1;$i<=$pages;$i++){
+        $fontsize=($now==$i)?"font-size:20px":"font-size:16px";
+        echo "<a href='?do=pop&p=$i' style='$fontsize'> $i </a>";
+    }
+    if(($now+1)<=$pages){
+        $next=$now+1;
+        echo "<a href='?do=pop&p=$next'> > </a>";
+    }
+    ?>
+    </div>    
 </fieldset>
