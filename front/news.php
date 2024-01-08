ff56573c5b79045786dd0abd11884c352398a4a4
mackliu <macklun@gmail.com>
2024-01-08 09:10:20 +0800

    建立最新文章和人氣的版型

---
+++ front/news.php
@@ -0,0 +1,15 @@
+<fieldset>
+    <legend>目前位置:首頁 > 最新文章區</legend>
+    <table style="width:95%;margin:auto">
+        <tr>
+            <th width="30%">標題</th>
+            <th width="50%">內容</th>
+            <th></th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</fieldset>
